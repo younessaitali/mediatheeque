24d8c4acf74a1ac2ffdf6a9861d6fc2f6a613951
younessaitali <younes.aitali95@gmail.com>
2019-03-30 18:55:26 +0100

    dev +mix

--- media/resources/views/layouts/nav_bar.blade.php
+++ media/resources/views/layouts/nav_bar.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body>
             <header>
