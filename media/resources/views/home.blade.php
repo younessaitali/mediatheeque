3707e060829b2951b10fab9b7f11c54f4003cbb1
GasmiSanae <GasmiSanae@users.noreply.github.com>
2019-05-02 20:55:50 +0100

    nothing works xD

--- media/resources/views/home.blade.php
+++ media/resources/views/home.blade.php
@@ -18,7 +18,21 @@
                         </div> <!-- end categorys -->
                 
                         <div class="ac-image">
-                            <img src="img/test.png" alt="hero image">
+                                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                        <div class="carousel-inner">
+                                            <div class="carousel-item active">
+                                                <img src={{$product->images[0]}} alt="product"></a>
+                                                </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                          <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                          <span class="sr-only">Next</span>
+                                        </a>
+                                      </div>
                         </div>
                     </div> <!-- end hero -->
 @endsection
